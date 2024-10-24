bf8cdc498d1a6f9901f78d3f7e2738c08eab16e1
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-10-24 14:47:50 +0530

    fix about page hero section

--- js/index.js
+++ js/index.js
@@ -1,42 +1,45 @@
-
 let toggle = false;
 // target element
-const moboicon = document.getElementById('menuToggle')
+const moboicon = document.getElementById("menuToggle");
 console.log(moboicon);
 
-const mobomenu = document.getElementById('mobileMenu')
+const mobomenu = document.getElementById("mobileMenu");
 console.log(mobomenu);
 
-
-moboicon.addEventListener('click', (e) => {
-    if (!toggle) {
-        mobomenu.style.transform = "translateX(0)"
-        toggle = true;
-    } else {
-        mobomenu.style.transform = "translateX(-400px)"
-        toggle = false;
-    }
-})
+moboicon.addEventListener("click", (e) => {
+  if (!toggle) {
+    mobomenu.style.transform = "translateX(0)";
+    toggle = true;
+  } else {
+    mobomenu.style.transform = "translateX(-400px)";
+    toggle = false;
+  }
+});
 
 // Counter
-const counters = document.querySelectorAll('.counter');
+const counters = document.querySelectorAll(".counter");
 
-  counters.forEach(counter => {
-    const updateCount = () => {
-      const target = +counter.getAttribute('data-target'); // Get target number
-      const count = +counter.innerText; // Current count value
-      const increment = target / 200; // Control the speed by dividing
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target"); // Get target number
+    const count = +counter.innerText; // Current count value
+    const increment = target / 200; // Control the speed by dividing
 
-      if (count < target) {
-        counter.innerText = Math.ceil(count + increment);
-        setTimeout(updateCount, 10); // Repeat every 10ms
-      } else {
-        counter.innerText = target; // Ensure it ends exactly at target
-      }
-    };
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 10); // Repeat every 10ms
+    } else {
+      counter.innerText = target; // Ensure it ends exactly at target
+    }
+  };
 
-    updateCount();
-  });
-  
-  
-  
+  updateCount();
+});
+
+document.addEventListener("click", function (event) {
+  // Check if the clicked target is not inside the menu or button
+  if (!mobomenu.contains(event.target) && !moboicon.contains(event.target)) {
+    mobomenu.style.transform = "translateX(-400px)";
+    toggle = false;
+  }
+});
